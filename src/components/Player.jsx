90d0deab96f49ebb4e822325c2c018d7343d1bbe
RahulQurbanov <156121142+RahulQurbanov@users.noreply.github.com>
2025-03-24 10:34:17 +0400

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,20 +2,20 @@ import { useRef, useState } from "react";
 
 export default function Player() {
 
-   const player = useRef()
-   const [playerName,setPlayerName] = useState()
+   const player = useRef();
+   const [playerName,setPlayerName] = useState();
 
    function showName(){
    setPlayerName(player.current.value)
-    player.current.value=''
-   }
+    player.current.value = '';
+   };
 
   return (
     <section id="player">
-      <h2>Welcome {playerName?playerName:"unknown entity"}</h2>
+      <h2>Welcome: {playerName?playerName:"unknown entity"}</h2>
       <p>
         <input ref={player} type="text"/>
-        <button onClick={showName}>Set Name</button>
+        <button onClick={showName}>Set-Name</button>
       </p>
     </section>
   );
